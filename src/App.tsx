470d101e77f47260cd7080b4a2e9852e581721bc
r70pro <f70pro@gmail.com>
2025-05-08 11:15:39 +1000

    Fix toast implementation by using Sonner instead of Radix UI toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
@@ -37,7 +36,6 @@ const AppContent = () => {
       <RouteChangeAnnouncer />
 
       <Toaster />
-      <Sonner />
       <Routes>
         {routes.map((route, index) => (
           <Route
